89e81b05beacdebe5bf0c483581996a84231dd6f
FIREcup <1039314612@qq.com>
2019-10-12 00:30:39 +0800

    locale

--- todoism/blueprints/home.py
+++ todoism/blueprints/home.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, current_app, jsonify, make_response
+from flask_login import current_user
+
+from ..extensions import db
 
 home_bp = Blueprint('home', __name__)
 
@@ -11,3 +14,18 @@ def index():
 @home_bp.route('/intro')
 def intro():
     return render_template('_intro.html')
+
+
+@home_bp.route('/set-locale/<locale>')
+def set_locale(locale):
+    if locale not in current_app.config['TODOISM_LOCALES']:
+        return jsonify(message=_('Invalid locale.')), 404
+
+    response = make_response(jsonify(message=_('Setting updated.')))
+    if current_user.is_authenticated:
+        current_user.locale = locale
+        db.session.commit()
+    else:
+        response.set_cookie('locale', locale, max_age=60 * 60 * 24 * 30)
+    return response
+
--- todoism/extensions.py
+++ todoism/extensions.py
@@ -1,7 +1,8 @@
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_babel import Babel
+from flask import request, current_app
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
@@ -15,3 +16,14 @@ login_manager.login_view = 'auth.login'
 def load_user(user_id):
     from todoism.models import User
     return User.query.get(int(user_id))
+
+
+@babel.localeselector
+def get_locale():
+    if current_user.is_authenticated and current_user.locale is not None:
+        return current_user.locale
+
+    locale = request.cookies.get('locale')
+    if locale is not None:
+        return locale
+    return request.accept_languages.best_match(current_app.config['TODOISM_LOCALES'])
