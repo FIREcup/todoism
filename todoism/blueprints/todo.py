c3b504e80313807f1cbae8f8ae960a5602a2998a
FIREcup <1039314612@qq.com>
2019-10-13 20:14:48 +0800

    locale

--- todoism/blueprints/todo.py
+++ todoism/blueprints/todo.py
@@ -66,3 +66,25 @@ def toggle_item(item_id):
     db.session.commit()
     return jsonify(message=_('Item toggled.'))
 
+
+@todo_bp.route('/item/<int:item_id>/delete', methods=['POST'])
+@login_required
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    if current_user != item.author:
+        return jsonify(message=_('Permission denied.')), 403
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify(message=_('Item deleted.'))
+
+
+@todo_bp.route('/item/clear', methods=['DELETE'])
+@login_required
+def clear_item():
+    items = Item.query.with_parent(current_user).filter_by(done=True).all()
+    for i in items:
+        db.session.delete(i)
+    db.session.commit()
+    return jsonify(message=_('Items cleared.'))
